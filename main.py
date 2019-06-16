dfd6f6952002d2c0d6c93ad2112d4266e0521ecc
Amey Kusurkar <amey1000@gmail.com>
2019-06-16 16:35:43 +0800

    Implement multiple spheres

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from PIL import Image
 import numpy as np
 # import pdb; pdb.set_trace()
 
-WIDTH, HEIGHT = 1024, 768
+WIDTH, HEIGHT = 400, 300
 FOV = (0.5) * np.pi
 framebuffer = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
+
+YELLOW = [255, 255, 0]
+BLUE = [0, 0, 255]
+RED = [255, 0, 0]
+GREEN = [0, 255, 0]
+BLACK = [0, 0, 0]
 
 class Ray:
     def __init__(self, p, v):
@@ -21,37 +27,57 @@ class Ray:
         return self.p + proj_vector
 
 class Sphere:
-    def __init__(self, center, radius):
+    def __init__(self, center, radius, color):
         self.center = center
         self.radius = radius
+        self.color = color
 
-    def ray_intersect(self, ray):
-        l = self.center - ray.p
-
+    def ray_intersect_dist(self, ray):
         projection = ray.projection(self.center)
 
-        if projection is None: return False
+        if projection is None: return None
 
-        dist = np.linalg.norm(projection - self.center)
+        projection_dist = np.linalg.norm(projection - self.center)
 
-        return dist <= self.radius
+        if projection_dist > self.radius:
+            return None
+        elif projection_dist == self.radius:
+            return projection_dist
+        else:
+            # TODO: There might be a performance optimisation here, by
+            # avoiding squaring `projection_dist`
+            proj_to_intersect_dist = np.sqrt(
+                np.square(self.radius) - np.square(projection_dist)
+            )
+            return np.linalg.norm(projection - ray.p) - proj_to_intersect_dist
 
-sphere = Sphere(np.array([-3, 0, -16]), 2)
+spheres = [
+    Sphere(np.array([-5, 0, -16]), 2, YELLOW),
+    Sphere(np.array([-3, 0, -18]), 4, BLUE),
+    Sphere(np.array([-1, -3, -16]), 3, GREEN),
+]
 
-def cast_ray(ray, sphere):
-    if sphere.ray_intersect(ray):
-        return [255, 255, 0]
-    else:
-        return [0, 0, 0]
+def cast_ray(ray, spheres):
+    color = BLACK
+    closest_dist = float("inf")
+    for sphere in spheres:
+        intersect_dist = sphere.ray_intersect_dist(ray)
+        if intersect_dist is None: continue
 
-for i in range(0, len(framebuffer)):
-    for j in range(0, len(framebuffer[i])):
+        if intersect_dist < closest_dist:
+            closest_dist = intersect_dist
+            color = sphere.color
+
+    return color
+
+for j in range(0, len(framebuffer)):
+    for i in range(0, len(framebuffer[j])):
         x =  (2 * (i + 0.5)/WIDTH  - 1) * np.tan(FOV/2) * WIDTH / HEIGHT
         y = -(2 * (j + 0.5)/HEIGHT - 1) * np.tan(FOV/2)
         v = np.array([x, y, -1])
         v = v / np.linalg.norm(v)
         ray = Ray(np.array([0, 0, 0]), v)
-        framebuffer[i, j] = cast_ray(ray, sphere)
+        framebuffer[j, i] = cast_ray(ray, spheres)
 
 im = Image.fromarray(framebuffer)
 im.save("test.png")
