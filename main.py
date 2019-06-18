ab8c5d426e66c30a4800a2e14f36de7f025977ac
Amey Kusurkar <amey1000@gmail.com>
2019-06-18 14:50:37 +0800

    Implement planes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 # import pdb; pdb.set_trace()
 
 WIDTH, HEIGHT = 400, 300
-FOV = (0.3) * np.pi
+FOV = (0.25) * np.pi
 framebuffer = np.zeros((HEIGHT, WIDTH, 3))
 
 YELLOW = [255, 255, 0]
@@ -33,10 +33,27 @@ class Ray:
         u = point - self.p
         uv_dot = np.dot(self.v, u)
         if uv_dot <= 0:
-            # Center of the circle does not project onto the ray
+            # Point does not project onto the ray
             return None
         proj_vector = (uv_dot / np.linalg.norm(ray.v)) * self.v
         return self.p + proj_vector
+
+class Plane:
+    def __init__(self, p0, normal, color, material):
+        self.p0 = p0
+        self.normal = normal
+        self.color = color
+        self.material = material
+
+    def ray_intersection(self, ray):
+        vn_dot = np.dot(ray.v, self.normal)
+        if vn_dot == 0: return None
+
+        intersect_dist = np.dot((self.p0 - ray.p), self.normal) / vn_dot
+        if intersect_dist < 0: return None
+
+        intersection = ray.p + (intersect_dist * ray.v)
+        return intersection, intersect_dist, self.normal
 
 class Sphere:
     def __init__(self, center, radius, color, material):
@@ -73,11 +90,12 @@ class Sphere:
         normal = normal / np.linalg.norm(normal)
         return intersection, intersect_dist, normal
 
-spheres = [
-    Sphere(np.array([-5, 4, -20]), 2, YELLOW, RUBBER),
-    Sphere(np.array([10, 3, -50]), 4, BLUE, MIRROR),
-    Sphere(np.array([-2, 5, -40]), 5, RED, IVORY),
-    Sphere(np.array([-1, 0, -10]), 1, GREEN, GLASS),
+objects = [
+    Sphere(np.array([-5, -2, -20]), 2, YELLOW, RUBBER),
+    Sphere(np.array([10, 0, -50]), 4, BLUE, MIRROR),
+    Sphere(np.array([-2, 1, -40]), 5, RED, IVORY),
+    Sphere(np.array([1, -2, -20]), 2, GREEN, GLASS),
+    Plane(np.array([0, -4, 0]), np.array([0, 1, 0]), GREEN, IVORY),
 ]
 
 lights = [
@@ -100,14 +118,14 @@ def refract(incident, normal, refractive_index):
     cos_r = np.sqrt(1 - np.square(refract_ratio) * (1 - np.square(cos_i)))
     return refract_ratio * incident + (refract_ratio * cos_i - cos_r) * normal
 
-def scene_intersection(ray, spheres):
+def scene_intersection(ray, objects):
     color = None
     closest_dist = float("inf")
     closest_intersection = None
     closest_normal = None
     material = None
-    for sphere in spheres:
-        result = sphere.ray_intersection(ray)
+    for obj in objects:
+        result = obj.ray_intersection(ray)
         if result is None: continue
 
         intersection, intersect_dist, normal = result
@@ -116,17 +134,17 @@ def scene_intersection(ray, spheres):
             closest_dist = intersect_dist
             closest_intersection = intersection
             closest_normal = normal
-            color = sphere.color
-            material = sphere.material
+            color = obj.color
+            material = obj.material
 
     if closest_intersection is None: return None
 
     return closest_intersection, closest_normal, color, material
 
-def cast_ray(ray, spheres, lights, depth=0):
+def cast_ray(ray, objects, lights, depth=0):
     if depth > REFLECT_DEPTH: return BG_COLOR
 
-    result = scene_intersection(ray, spheres)
+    result = scene_intersection(ray, objects)
     if result is None: return BG_COLOR
     intersection, normal, color, material = result
 
@@ -141,7 +159,7 @@ def cast_ray(ray, spheres, lights, depth=0):
         offset = 1e-3 * normal
         ln_dot = np.dot(light_dir, normal)
         shadow_p = intersection - offset if ln_dot < 0 else intersection + offset
-        if scene_intersection(Ray(shadow_p, light_dir), spheres): continue
+        if scene_intersection(Ray(shadow_p, light_dir), objects): continue
 
         diffuse_intensity += light.intensity * max(0, ln_dot)
         rv_dot = max(0, np.dot(-reflect(-light_dir, normal), ray.v))
@@ -150,12 +168,12 @@ def cast_ray(ray, spheres, lights, depth=0):
     reflect_dir = reflect(ray.v, normal)
     reflect_dir = reflect_dir / np.linalg.norm(reflect_dir)
     reflect_p = intersection - offset if np.dot(reflect_dir, normal) < 0 else intersection + offset
-    reflect_color = cast_ray(Ray(reflect_p, reflect_dir), spheres, lights, depth + 1)
+    reflect_color = cast_ray(Ray(reflect_p, reflect_dir), objects, lights, depth + 1)
 
     refract_dir = refract(ray.v, normal, material.refractive_index)
     refract_dir = refract_dir / np.linalg.norm(refract_dir)
     refract_p = intersection - offset if np.dot(refract_dir, normal) < 0 else intersection + offset
-    refract_color = cast_ray(Ray(refract_p, refract_dir), spheres, lights, depth + 1)
+    refract_color = cast_ray(Ray(refract_p, refract_dir), objects, lights, depth + 1)
 
     intensity = diffuse_intensity * np.array(color) * material.diffuse_reflection + \
                 specular_intensity * np.array([255, 255, 255]) * material.specular_reflection + \
@@ -171,7 +189,7 @@ for j in range(0, len(framebuffer)):
         v = np.array([x, y, -1])
         v = v / np.linalg.norm(v)
         ray = Ray(np.array([0, 0, 0]), v)
-        framebuffer[j, i] = cast_ray(ray, spheres, lights)
+        framebuffer[j, i] = cast_ray(ray, objects, lights)
 
 for j in range(0, len(framebuffer)):
     for i in range(0, len(framebuffer[j])):
