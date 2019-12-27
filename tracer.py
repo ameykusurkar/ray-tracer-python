5e9c8d1f63addb6e938e5134defb6d7a2b5b810e
Amey Kusurkar <amey1000@gmail.com>
2019-12-27 18:34:41 +0000

    Generate view of background with gradient

--- tracer.py
+++ tracer.py
@@ -1,12 +1,28 @@
 from PIL import Image
 import numpy as np
 
-WIDTH, HEIGHT = 300, 200
+WIDTH, HEIGHT = 200, 100
 
-foo = np.zeros((HEIGHT, WIDTH, 3))
-foo[:, :, 0] = np.linspace(0, 255, WIDTH)
-foo[:, :, 1] = np.linspace(255, 0, HEIGHT).reshape(-1, 1)
-foo[:, :, 2] = 0.2
+def normalize(v):
+    last_dim = v.ndim - 1
+    v_mag = np.linalg.norm(v, axis=last_dim, keepdims=True)
+    return np.divide(v, v_mag, where=(v_mag > 0))
 
-im = Image.fromarray(foo.astype(np.uint8))
+def color(ray_p, ray_dir):
+    unit_direction = normalize(ray_dir)
+    t = 0.5 * (unit_direction[:, :, 1] + 1)
+    t = t[..., np.newaxis]
+    return (1 - t) * [1.0, 1.0, 1.0] + t * [0.5, 0.7, 1.0]
+
+ray_dir = np.zeros((HEIGHT, WIDTH, 3))
+ray_p = np.zeros((HEIGHT, WIDTH, 3))
+
+ray_dir[:, :, 0] = np.linspace(0, 1, WIDTH)
+# We want the the y-coord to increase upwards
+ray_dir[:, :, 1] = np.linspace(1, 0, HEIGHT).reshape(-1, 1)
+ray_dir[:, :, 2] = -1
+
+pixels = color(ray_p, ray_dir) * 255
+
+im = Image.fromarray(pixels.astype(np.uint8))
 im.save("output.png")
