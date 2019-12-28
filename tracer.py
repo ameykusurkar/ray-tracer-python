82aaa06d72d66411dca4d22886ed04175d33a232
Amey Kusurkar <amey1000@gmail.com>
2019-12-28 13:56:33 +0000

    Change dimensions of rays vector
    Change from H x W x 3 to (H x W) x 3. The computation is equivalent, but
    simplifies by reducing a dimension.

--- tracer.py
+++ tracer.py
@@ -10,19 +10,25 @@ def normalize(v):
 
 def color(ray_p, ray_dir):
     unit_direction = normalize(ray_dir)
-    t = 0.5 * (unit_direction[:, :, 1] + 1)
+    t = 0.5 * (unit_direction[:, 1] + 1)
     t = t[..., np.newaxis]
     return (1 - t) * [1.0, 1.0, 1.0] + t * [0.5, 0.7, 1.0]
 
 ray_dir = np.zeros((HEIGHT, WIDTH, 3))
-ray_p = np.zeros((HEIGHT, WIDTH, 3))
 
 ray_dir[:, :, 0] = np.linspace(0, 1, WIDTH)
 # We want the the y-coord to increase upwards
 ray_dir[:, :, 1] = np.linspace(1, 0, HEIGHT).reshape(-1, 1)
 ray_dir[:, :, 2] = -1
 
-pixels = color(ray_p, ray_dir) * 255
+# Even though the pixels have the dimensions H x W x 3, we can think of
+# them as (H x W) number of rays, reducing the number for dimensions
+# while calculating.
+ray_dir = ray_dir.reshape(-1, 3)
+ray_p = np.zeros((HEIGHT * WIDTH, 3))
 
-im = Image.fromarray(pixels.astype(np.uint8))
+pixels = color(ray_p, ray_dir) * 255
+pixels = pixels.reshape(HEIGHT, WIDTH, 3).astype(np.uint8)
+
+im = Image.fromarray(pixels)
 im.save("output.png")
